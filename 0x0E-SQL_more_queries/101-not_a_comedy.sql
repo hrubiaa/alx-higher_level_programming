56835cc46826d847427d74635cc24956204dd364
hrubiaa <hestonrubia01@gmail.com>
2022-08-11 15:22:09 -0700

    queries

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating
+-- Records are ordered by ascending order of the rating
+SELECT title
+FROM tv_shows
+WHERE title NOT IN
+(SELECT title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy')
+GROUP BY title
+ORDER BY title asc;
